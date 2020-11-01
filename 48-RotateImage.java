3ff7febb8751f6978d132e7a90df15ff10cd607d
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-11-01 12:53:04 +0800

    48-Rotate Image

---
+++ 48-RotateImage.java
@@ -0,0 +1,22 @@
+class Solution48 {
+    public void rotate(int[][] matrix) {
+        int s = 0, e = matrix.length - 1;
+        // reverse up to down
+        while (s < e) {
+            int[] temp = matrix[s];
+            matrix[s] = matrix[e];
+            matrix[e] = temp;
+            s++;
+            e--;
+        }
+
+        // swap the symmetry
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
