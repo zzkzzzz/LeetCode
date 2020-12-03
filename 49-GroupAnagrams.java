65180fe2e9de236a0a4aa54468cde0491dca404b
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-12-03 16:44:30 +0800

    49-GroupAnagrams

---
+++ 49-GroupAnagrams.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = new char[26];
+            for (char c : s.toCharArray())
+                arr[c - 'a']++;
+            String key = String.valueOf(arr);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
