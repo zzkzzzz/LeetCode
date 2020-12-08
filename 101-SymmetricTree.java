943ace3c4db821980441824c7bb6ef4c879ad961
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-12-09 01:30:24 +0800

    101-SymmetricTree

---
+++ 101-SymmetricTree.java
@@ -0,0 +1,34 @@
+class Solution101 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricHelp(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+        // preorder traverse
+        if (left == null || right == null)
+            return left == right;
+        if (left.val != right.val)
+            return false;
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
+    }
+
+}
