1cccab6c4fc1d26ffd9fa86e5f2736cb7b5e1e37
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-12-12 17:52:10 +0800

    199-Binary Tre eRight Side View

---
+++ 199-BinaryTreeRightSideView.java
@@ -0,0 +1,78 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution199 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth) {
+        if (curr == null) {
+            return;
+        }
+
+        // only need the right most node
+        // each level will only add one node
+        // after that list size will increase
+        if (currDepth == result.size()) {
+            result.add(curr.val);
+        }
+
+        // the traverse of the tree is NOT standard pre-order traverse. It checks the
+        // RIGHT node first and then the LEFT
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+
+    }
+
+    // ;evel order traversal, and add the last node on every layer.
+    public List<Integer> rightSideView2(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        List<Integer> res = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+
+                if (size == 0)
+                    res.add(cur.val);
+
+                if (cur.left != null)
+                    queue.offer(cur.left);
+                if (cur.right != null)
+                    queue.offer(cur.right);
+            }
+        }
+
+        return res;
+    }
+}
