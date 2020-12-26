a201f9aef9bd2cee92ce2055d1a24870048e3a09
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-12-26 22:50:52 +0800

    669. Trim a Binary Search Tree

---
+++ 669-TrimaBinarySearchTree.java
@@ -0,0 +1,38 @@
+class Solution669 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return null;
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+
+        if (root.val < low)
+            return root.right;
+
+        if (root.val > high)
+            return root.left;
+
+        return root;
+
+    }
+}
