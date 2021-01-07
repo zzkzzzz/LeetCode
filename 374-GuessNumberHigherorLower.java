94bed34512969894e037ff292880734510acee05
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-01-07 18:27:04 +0800

    374. Guess Number Higher or Lower

---
+++ 374-GuessNumberHigherorLower.java
@@ -0,0 +1,24 @@
+class Solution374 {
+    int guess(int num) {
+        return num;
+    }
+
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == -1)
+                right = mid - 1;
+            else if (guess(mid) == 1)
+                left = mid + 1;
+
+        }
+
+        return -1;
+    }
+}
