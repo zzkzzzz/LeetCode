ced5ea4ff02f18cc7f3474c1c28f4be1e68e5592
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-01-10 00:35:55 +0800

    876-Middle of the LinkedList

---
+++ 876-MiddleoftheLinkedList.java
@@ -0,0 +1,30 @@
+class Solution876 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    ListNode middleNode(ListNode head) {
+        ListNode fast, slow;
+        fast = slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        // slow 就在中间位置
+        return slow;
+    }
+}
