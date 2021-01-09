b4a115bc0c6b399f43f437084a4e10e341b47b88
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-01-10 00:35:47 +0800

    344-Reverse String

---
+++ 344-ReverseString.java
@@ -0,0 +1,14 @@
+class Solution344 {
+    public void reverseString(char[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            // 交换 arr[left] 和 arr[right]
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
