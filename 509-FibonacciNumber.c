6fca2606e0b7513a7f33f26454deb5ff577bb44b
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-02-15 15:46:44 +0800

    added solution

--- 509-FibonacciNumber.c
+++ 509-FibonacciNumber.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+// dp bottom up with dp table
+int fib(int N)
+{
+    if (N < 1)
+        return 0;
+    if (N == 1 || N == 2)
+        return 1;
+
+    int dp[N + 1];
+    // base case
+    dp[1] = dp[2] = 1;
+
+    for (int i = 3; i <= N; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[N];
+}
+
+// futher optimization to 2 variables
 // dp bottom up
 int fib(int n)
 {
